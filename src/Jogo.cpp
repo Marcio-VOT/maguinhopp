bf1b55ce2d3b0244d13d78213084c8b57a20ca7d
Paulo Victor Souza de Almeida <67125342+PauloV-Almeida@users.noreply.github.com>
2023-11-07 13:01:28 -0300

    gerenciadorGrafico

--- src/Jogo.cpp
+++ src/Jogo.cpp
@@ -3,14 +3,24 @@
 Jogo::Jogo() : pGrafico(pGrafico->getGerenciadorGrafico()),
                personagens()
 {
+    if(pGrafico == nullptr)
+    {
+        std::cout << "Erro ao criar janela" << std::endl;
+        exit(1);
+    }
+
+
     Jogador* jogador = new Jogador(Vector2f(100.0f, 200.0f), Vector2f(50.0f, 50.0f));
-    Inimigo* inimigo = new Inimigo(Vector2f(100.0f, 100.0f), Vector2f(50.0f, 50.0f), jogador);
+    Inimigo* inimigo1 = new Inimigo(Vector2f(100.0f, 100.0f), Vector2f(50.0f, 50.0f), jogador);
+    Inimigo* inimigo2 = new Inimigo(Vector2f(200.0f, 200.0f), Vector2f(50.0f, 50.0f), jogador);
 
     Personagem* p1 = static_cast<Personagem*>(jogador);
-    Personagem* p2 = static_cast<Personagem*>(inimigo);
+    Personagem* p2 = static_cast<Personagem*>(inimigo1);
+    Personagem* p3 = static_cast<Personagem*>(inimigo2);
 
     personagens.push_back(p1);
     personagens.push_back(p2);
+    personagens.push_back(p3);
 
     Executar();
 }
@@ -21,21 +31,30 @@ Jogo::~Jogo()
 
 void Jogo::Executar()
 {
-    while (window.isOpen())
+    while (pGrafico->verificaJanelaAberta())
     {
         Event event;
-        while (window.pollEvent(event))
+        while (pGrafico->getWindow()->pollEvent(event))
         {
             if (event.type == Event::Closed)
-                window.close();
+            {
+                pGrafico->fechaJanela();
+            }else if(event.type == Event::KeyPressed)
+            {
+                if(event.key.code == Keyboard::Escape)
+                {
+                    pGrafico->fechaJanela();
+                }
+            }
+                
         }
-        window.clear();
+        pGrafico->limpaJanela();
         for(int i = 0; i < personagens.size(); i++)
         {
             personagens.at(i)->move();
-            window.draw(personagens.at(i)->getBody());
+            pGrafico->desenhaElemento(personagens.at(i)->getBody());
         }
-        window.display();
+        pGrafico->mostraElemento();
     }
     personagens.clear();
 }
